11e65b85743e3da1b9e5675f963767a318a1e8ea
Danny Garcia <contact@danny-garcia.com>
2013-12-04 00:01:42 -0800

    random walks

--- introduction/main.js
+++ introduction/main.js
@@ -3,10 +3,34 @@
 	var canvas = document.querySelector("#canvas"),
 		ctx = canvas.getContext("2d"),
 		width = canvas.width,
-		height = canvas.height;
+		height = canvas.height,
+		cursor = { x: 0, y: 0 };
+
+	document.onmousemove = function (e) {
+		cursor.x = e.pageX;
+		cursor.y = e.pageY;
+	};
 
 	function point(x, y) {
 		ctx.fillRect(x, y, 1, 1);
+	}
+
+	function Random() {
+		return {
+			random : Math.random,
+			sqrt : Math.sqrt,
+			log : Math.log,
+			nextGaussian : function () {
+				var x1, x2, rad, y1;
+				do {
+					x1 = 2 * this.random() - 1;
+					x2 = 2 * this.random() - 1;
+					rad = x1 * x1 + x2 * x2;
+				} while (rad >= 1 || rad === 0);
+				var c = this.sqrt(-2 * this.log(rad) / rad);
+				return x1 * c;
+			}
+		};
 	}
 
 	function random(min, max) {
@@ -28,43 +52,77 @@
 			display : function () {
 				point(this.x, this.y);
 			},
-			step : function () {
-				var stepx = random(-1, 1);
-				var stepy = random(-1, 1);
-				this.x += stepx;
-				this.y += stepy;
+			step : function (target) {
+
+				var r = Math.random();
+
+				if (r < 0.01) {
+					this.x += random(-100, 100);
+					this.y += random(-100, 100);
+				} else {
+					this.x += random(-1, 1);
+					this.y += random(-1, 1);
+				}
+
+				/*
+				if (r < 0.5) {
+					if (target.x > this.x) {
+						this.x++;
+					} else {
+						this.x--;
+					}
+					if (target.y > this.y) {
+						this.y++;
+					} else {
+						this.y--;
+					}
+				} else if (r < 0.5 + 0.125) {
+					this.x++;
+				} else if (r < 0.5 + (0.125 * 2)) {
+					this.x--;
+				} else if (r < 0.5 + (0.125 * 3)) {
+					this.y++;
+				} else {
+					this.y--;
+				}
+				*/
+
 			}
 		};
 	};
 
 	var walkers = [new Walker(random(100), random(100)), new Walker(random(-100), random(-100)), new Walker()],
 		randomCounts = [],
-
-		i, w, x;
+		generator = new Random(),
+		sd = Math.min(width, height) / 8, meanW = width / 2, meanH = height / 2, numX, numY,
+		i, x;
 
 	ctx.fillStyle = "rgba(0, 255, 0, 0.7)";
 
-	for (i = 0; i < 20; i++) {
-		randomCounts[i] = 0;
-	}
+	// for (i = 0; i < 20; i++) {
+	// 	randomCounts[i] = 0;
+	// }
 
-	w = width / randomCounts.length;
+	ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
 
 	function loop() {
-		ctx.fillStyle = "rgba(0,0,0,0.2)";
-		ctx.fillRect(0, 0, width, height);
+		// ctx.fillStyle = "rgba(0,0,0,0.1)";
+		// ctx.fillRect(0, 0, width, height);
 
-		i = random(randomCounts.length);
-		randomCounts[i]++;
 
-		ctx.fillStyle = "rgba(0, 255, 0, 0.7)";
+		// randomCounts[i]++;
+		numX = generator.nextGaussian();
+		numY = generator.nextGaussian();
+
 		for (i = 0; i < walkers.length; i++) {
+
 			walkers[i].display();
-			walkers[i].step();
+			walkers[i].step({
+				x: (sd * numX + meanW),
+				y: (sd * numY + meanH)
+			});
 		}
-		for (x = 0; x < randomCounts.length; x++) {
-			ctx.fillRect(x * w, height - randomCounts[x], w - 1, randomCounts[x]);
-		}
+
 		window.requestAnimationFrame(loop);
 	}
 
